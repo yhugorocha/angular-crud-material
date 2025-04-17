78a76afcfb1657af55c2ca951df5e12744642d2d
Hugo Silva <jose.silva@extreme.digital>
2025-04-17 15:44:01 -0300

    feat: Creating a toobar, adding the toobar image and adding the new routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+      RouterModule,
+      MatToolbarModule
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
